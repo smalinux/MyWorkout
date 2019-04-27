07fea23cce2f27ca043d6cb397a54b4145a1f354
Sohaib Mohammed <sohaibfacebook396@gmail.com>
2019-04-27 20:00:11 +0200

    up

---
+++ Bash-Scripting.sh
@@ -0,0 +1,14 @@
+"Cheat sheet"
+
+# Chapter 3. Special Characters
+###############################################
+https://www.tldp.org/LDP/abs/html/special-chars.html
+
+> almost every symbol is a Special Character
+> 
+>
+>>>
+
+
+# Chapter 4. Introduction to Variables and Parameters
+###############################################
---
+++ myBashScripts/listAllExecFile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# list 'alll' execuatable files in ur sys
+
+ls /bin  >> ~smalinux/temp
+ls /sbin  >> ~smalinux/temp
+ls /usr/bin/  >> ~smalinux/temp
+ls /usr/sbin/  >> ~smalinux/temp
+ls /usr/local/bin/  >> ~smalinux/temp
+ls /usr/local/sbin/ >> ~smalinux/temp
